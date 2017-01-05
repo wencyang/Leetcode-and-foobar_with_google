4bd896e7150dac3cd61fe0a70022c0081dc172d7
Wencao Yang <yangwencao@gmail.com>
2017-01-04 19:03:16 -0500

    easy

---
+++ Romen Number to Integer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def romanToInt(self, s):
+        l=list(s)
+        dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        sum=0
+        for rn in l:
+            sum=sum+dic[rn]
+        for i in range(len(l)-1):
+            if dic[l[i]]<dic[l[i+1]]:
+                sum=sum-2*dic[l[i]]
+        return sum
+            
