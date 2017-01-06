d85065be264e5a8b13f8a5e507d0c9306529c4e1
Wencao Yang <yangwencao@gmail.com>
2017-01-05 22:44:30 -0500

    Add files via upload

---
+++ longest_substring_without_repeating characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start=0
+        char={}
+        maxlength=0
+        for i in range(len(s)):
+            if s[i] in char and start<=char[s[i]]:
+                start=char[s[i]]+1
+            else:
+                maxlength=max(maxlength,i-start+1)
+            char[s[i]]=i
+        return maxlength
