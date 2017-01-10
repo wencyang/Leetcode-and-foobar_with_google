288eb8b121357ca577ef97d10ded31c3a93885ec
Wencao Yang <wencyang@indiana.edu>
2017-01-09 22:50:09 -0500

    medium

---
+++ rotate_image.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n=len(matrix)
+        for i in range(n/2):
+            for j in range(n/2+n%2):
+                a=matrix[j][n-i-1]
+                matrix[j][n-i-1]=matrix[i][j]
+                matrix[i][j]=matrix[n-j-1][i]
+                matrix[n-j-1][i]=matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1]=a
+                
