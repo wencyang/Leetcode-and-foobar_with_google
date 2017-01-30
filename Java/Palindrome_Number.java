aad11ccbe3ce1908fb5627a896ca077d60ac3aa3
Wencao Yang <wencyang@indiana.edu>
2017-01-29 19:12:39 -0500

    Update Palindrome_Number.java

--- Java/Palindrome_Number.java
+++ Java/Palindrome_Number.java
@@ -1,1 +1,19 @@
+// determin if a number is a palindrome or not
+public class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+    
+    if (x < 0) return false;
 
+    int p = x; 
+    int q = 0; 
+    
+    while (p > 0){
+        q *=10; 
+        q += p%10; 
+        p /=10; 
+    }
+    
+    return q == x ;
+        
+    }
+}
